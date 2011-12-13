d06d6a73e630abe3a5baab5799c3253e1c5c6b9e
jose.gonzalez <jose.gonzalez@rudelabs.com>
2011-12-13 12:53:42 +0100

    Kiwi (BDD) added with example test

---
+++ iSomUPC/iSomUPCTests/classes/UPCKiwiTest.m
@@ -0,0 +1,21 @@
+//
+//  UPCKiwiTest.m
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 13/12/11.
+//  Copyright (c) 2011 Rude Labs. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(UPCKiwiTest)
+
+describe(@"BDD", ^{
+    it(@"is cool", ^{
+        NSUInteger a = 12;
+        NSUInteger b = 16;
+        [[theValue(a+b) should] equal:theValue(28)];
+    });
+});
+
+SPEC_END
