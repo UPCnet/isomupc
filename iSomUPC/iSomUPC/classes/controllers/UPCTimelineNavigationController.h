4681d27268c2cc1db133f7d79b6baefea46124c9
jose.gonzalez <jose.gonzalez@rudelabs.com>
2012-01-08 15:13:23 +0100

    Add navigation controller as root controller of first tab

---
+++ iSomUPC/iSomUPC/classes/controllers/UPCTimelineNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  UPCTimelineNavigationController.h
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 08/01/12.
+//  Copyright (c) 2012 UPCnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UPCTimelineNavigationController : UINavigationController
+
+@end
