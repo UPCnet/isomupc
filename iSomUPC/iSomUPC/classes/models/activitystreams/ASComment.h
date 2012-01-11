b668fdd3ee7bba36c341a9bdfe4d7a803ada21e8
jose.gonzalez <jose.gonzalez@rudelabs.com>
2012-01-11 10:47:14 +0100

    New class for comments

---
+++ iSomUPC/iSomUPC/classes/models/activitystreams/ASComment.h
@@ -0,0 +1,24 @@
+//
+//  ASComment.h
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 11/01/12.
+//  Copyright (c) 2012 UPCnet. All rights reserved.
+//
+
+#import "ASObject.h"
+
+
+@interface ASComment : ASObject
+
+@property (strong, nonatomic) NSString *id;
+
+@property (strong, nonatomic) ASObject *author;
+@property (strong, nonatomic) NSString *displayName;
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSArray *inReplyTo;
+
+@property (strong, nonatomic) NSDate *published;
+@property (strong, nonatomic) NSDate *updated;
+
+@end
---
+++ iSomUPC/iSomUPC/classes/models/activitystreams/ASComment.m
@@ -0,0 +1,22 @@
+//
+//  ASComment.m
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 11/01/12.
+//  Copyright (c) 2012 UPCnet. All rights reserved.
+//
+
+#import "ASComment.h"
+
+
+@implementation ASComment
+
+@synthesize id;
+@synthesize author;
+@synthesize displayName;
+@synthesize content;
+@synthesize inReplyTo;
+@synthesize published;
+@synthesize updated;
+
+@end
