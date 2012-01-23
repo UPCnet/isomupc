38718f8e46791b0a5bab3ec8371178597b3fca8e
jose.gonzalez <jose.gonzalez@rudelabs.com>
2012-01-23 10:57:26 +0100

    Custom UITableViewCell for timeline view

---
+++ iSomUPC/iSomUPC/classes/views/UPCTimelineViewCell.h
@@ -0,0 +1,20 @@
+//
+//  UPCTimelineViewCell.h
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 23/01/12.
+//  Copyright (c) 2012 UPCnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UPCTimelineViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) IBOutlet UILabel *displayNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *activityLabel;
+@property (strong, nonatomic) IBOutlet UILabel *relativeDateLabel;
+@property (strong, nonatomic) IBOutlet UILabel *numberOfCommentsLabel;
+
+@end
---
+++ iSomUPC/iSomUPC/classes/views/UPCTimelineViewCell.m
@@ -0,0 +1,20 @@
+//
+//  UPCTimelineViewCell.m
+//  iSomUPC
+//
+//  Created by Jose Gonzalez Gomez on 23/01/12.
+//  Copyright (c) 2012 UPCnet. All rights reserved.
+//
+
+#import "UPCTimelineViewCell.h"
+
+
+@implementation UPCTimelineViewCell
+
+@synthesize avatarImageView;
+@synthesize displayNameLabel;
+@synthesize activityLabel;
+@synthesize relativeDateLabel;
+@synthesize numberOfCommentsLabel;
+
+@end
